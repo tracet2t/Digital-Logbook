33eb52f7030f04cc75b9167e76408ca287f10b63
Charan Romi <charanromi325@gmail.com>
2024-08-26 15:25:05 +0530

    86b1vnx5r:Added Jest testing

---
+++ server/test/sum.test.ts
@@ -0,0 +1,8 @@
+import sum from './sum';
+import { describe, expect, test } from '@jest/globals';
+
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
---
+++ server/test/sum.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number): number {
+    return a + b;
+  }
+  
+export default sum;
+  
