dc2f06b183e87b9a4242678f2950025280548d82
Mohmaed Asfik <Asfikforever@gmail.com>
2024-08-29 18:35:23 +0530

    86b1vx0v2: Hide the logout button

--- server/src/app/page.tsx
+++ server/src/app/page.tsx
@@ -6,10 +6,14 @@ export default function Home() {
   return (
     <div className="p-2">
       <div className="flex gap-2 justify-between">
-        <Title level={3}>Dashboard</Title>
-        <form action="/auth/logout" method="post">
+        <Title level={3} className="mt-12"> {/* Adjust margin-top to move the title down */}
+          Dashboard
+        </Title>
+
+        {/* Uncomment and use this form for logout if needed */}
+        {/* <form action="/auth/logout" method="post">
           <Button htmlType="submit">Logout</Button>
-        </form>
+        </form> */}
       </div>
       <TaskCalendar />
     </div>
