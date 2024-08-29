3631d90c4984b46b03b0ca88d927a33d72c17e1e
thlurte <s92062851@ousl.lk>
2024-08-29 09:29:53 +0530

    86b1u133x: role based routing and rendering

--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -1,19 +1,48 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    console.log("Middleware called");
+  console.log('Middleware called');
 
-    // verify authentication cookie here (JWT or any other method)
-    
-    let cookie = request.cookies.get("token");
-    if (!cookie || cookie.value != 'authenticated') {
-        return NextResponse.redirect(`${process.env.BASE_URL}/login`);
+  const token = request.cookies.get('token');
+  const userRole = 'student'; 
+  
+
+
+  if (!token || token.value !== 'authenticated') {
+    return NextResponse.redirect(`${process.env.BASE_URL}/login`);
+  }
+
+  const { pathname } = request.nextUrl;
+
+  if (userRole === 'student') {
+    if (pathname.startsWith('/mentor')) {
+      return NextResponse.redirect(`${process.env.BASE_URL}/unauthorized`);
     }
-    return NextResponse.next();
+    if (pathname === '/unauthorized') {
+      return NextResponse.next();
+    }
+    // if (pathname !== '/student') {
+    //   return NextResponse.redirect(`${process.env.BASE_URL}/student`);
+    // }
+  }
+
+  if (userRole === 'mentor') {
+    if (pathname.startsWith('/student')) {
+      return NextResponse.redirect(`${process.env.BASE_URL}/unauthorized`);
+    }
+    if (pathname === '/unauthorized') {
+      return NextResponse.redirect(`${process.env.BASE_URL}/mentor`);
+    }
+    // if (pathname !== '/mentor') {
+    //   return NextResponse.redirect(`${process.env.BASE_URL}/mentor`);
+    // }
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-      '/((?!login|auth/login|_next/static|_next/image|favicon.ico).*)',
-    ],
-  }
+  matcher: [
+    '/((?!login|auth/login|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
---
+++ server/src/pages/dashboard.tsx
@@ -0,0 +1,32 @@
+import { GetServerSideProps } from 'next';
+import StudentPage from './student';
+import MentorPage from './mentor';
+import UnauthorizedPage from './unauthorized';
+
+interface DashboardProps {
+  userRole: string;
+}
+
+const Dashboard: React.FC<DashboardProps> = ({ userRole }) => {
+  return (
+    <div>
+      {userRole === 'mentor' && <MentorPage />}
+      {userRole === 'student' && <StudentPage />}
+      {userRole !== 'mentor' && userRole !== 'student' && <UnauthorizedPage />}
+    </div>
+  );
+};
+
+
+// i am assuming that during the login phase the user role and id is stored in cookies
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const userRole = context.req.cookies['userRole'] || 'guest';
+
+  return {
+    props: {
+      userRole,
+    },
+  };
+};
+
+export default Dashboard;
