d8277a6bb763cb9b8ca3558c5304998cf262475c
Mohamed shazan <shaloste@gmail.com>
2024-08-30 16:50:01 +0530

    86b1u0xkk :  JWT modifyed  authentication

--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -1,14 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
+import * as jose from 'jose'
 
-export function middleware(request: NextRequest) {
+
+
+export async function middleware(request: NextRequest) {
     console.log("Middleware called");
 
-    // verify authentication cookie here (JWT or any other method)
-    
-    let cookie = request.cookies.get("token");
-    if (!cookie || cookie.value != 'authenticated') {
-        return NextResponse.redirect(`${process.env.BASE_URL}/login`);
-    }
+    let token = request.cookies.get("token");
+
+    if (!token) {
+      return NextResponse.redirect(`${process.env.BASE_URL}/login`);
+  }
+
+
+    try {
+            const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+            const {payload, protectedHeader} = await jose.jwtVerify(token.value, secret);
+
+        } catch (error) {
+        
+            console.error(error);
+            
+            const headers = new Headers(request.headers);
+
+            if ((error as any).code == "ERR_JWT_EXPIRED") {
+                headers.set("Set-Cookie", "redirect_error=Session expired. Please login again.; Path=/login;");
+            }
+
+            headers.set("Set-Cookie", "redirect_error=An error occurred while logging you in.; Path=/login;");
+            headers.append("Set-Cookie", `token=${token}; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/; HttpOnly`);
+            return NextResponse.redirect(`${process.env.BASE_URL}/login`, { status: 303, headers: headers });
+        }
     return NextResponse.next();
 }
 
