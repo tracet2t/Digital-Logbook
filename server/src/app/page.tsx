01f69288a172d88c9030fd8def0a2a79ba949774
Mohmaed Asfik <Asfikforever@gmail.com>
2024-09-11 11:20:47 +0530

    Remove antd

--- server/src/app/page.tsx
+++ server/src/app/page.tsx
@@ -1,7 +1,7 @@
 // src/app/page.tsx
 import TaskCalendar from "@/components/calendar";
-import { Button } from "antd";
-import Title from "antd/es/typography/Title";
+// import { Button } from "antd";
+// import Title from "antd/es/typography/Title";
 import { getSession } from "@/server_actions/getSession";
 
 export default async function Home() {
@@ -11,7 +11,7 @@ export default async function Home() {
   return (
     <div className="p-6 h-[80vh] gap-4"> {/* Increased padding and set height to 80% of viewport height */}
       <div className="flex flex-col gap-8"> {/* Increased gap for more spacing */}
-        <Title level={3} className="mb-8">Dashboard</Title> {/* Increased margin-bottom */}
+        {/* <Title level={3} className="mb-8">Dashboard</Title> Increased margin-bottom */}
         {/* <form action="/auth/logout" method="post">
           <Button htmlType="submit">Logout</Button>
         </form> */}
