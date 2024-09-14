5fc9cbd36ce66751018d85c23cc4f28c06122ec6
P.K.U.Devaki <s92062603@ousl.lk>
2024-09-14 17:35:26 +0530

    86b256a3c: Add the back text on empty button

--- server/src/app/mentor/bulkreport/page.tsx
+++ server/src/app/mentor/bulkreport/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'; 
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
@@ -12,12 +12,7 @@ import {
 } from "@/components/ui/table";
 
 import { Button } from '@/components/ui/button';
-import { list } from 'postcss';
 import { Parser } from 'json2csv';
-
-
-
-
 
 const BulkReports: React.FC = () => {
   const router = useRouter(); 
@@ -113,9 +108,6 @@ const handleReport = async (report: Report): Promise<void> => {
   }
 };
 
-
-
-
   useEffect(() => {
     const fetchReports = async () => {
       try {
@@ -140,12 +132,15 @@ const handleReport = async (report: Report): Promise<void> => {
   return (
     <div className="p-8 bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 min-h-screen flex flex-col items-center">
       <div className="w-full max-w-6xl flex items-center mb-6">
+        {/* Back button to navigate to the previous page */}
         <button
           onClick={() => router.back()}
           className="bg-blue-500 text-white px-4 py-2 rounded-md flex items-center hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
-          <span className="mr-2 font-bold">&larr;</span> 
+          <span className="mr-2 font-bold">&larr;</span> Back
         </button>
+
+        
       </div>
       <h1 className="text-3xl font-bold text-gray-800 mb-10 mt-1 text-center">Bulk Reports</h1>
       {loading && <p className="text-blue-500">Loading reports...</p>}
@@ -183,7 +178,7 @@ const handleReport = async (report: Report): Promise<void> => {
                     </TableCell>
                     <TableCell className="p-4">
                       {report.reportData ? (
-                        <Button onClick={()=> {handleReport(report)}} > Download </Button>
+                        <Button onClick={() => { handleReport(report) }}> Download </Button>
                       ) : (
                         <span className="text-gray-500">N/A</span>
                       )}
