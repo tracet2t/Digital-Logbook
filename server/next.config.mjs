e252f6f3622da9f2d4dbc00d762dc881e1824058
thlurte <s92062851@ousl.lk>
2024-09-24 09:29:02 +0530

    3f5a9a7kq: containerized next js

--- server/next.config.mjs
+++ server/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-  };
+  reactStrictMode: false,
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
