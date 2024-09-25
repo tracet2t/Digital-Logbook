fce721236a6b3a4b5eca19cbed9a0ef5ba42d110
thlurte <s92062851@ousl.lk>
2024-09-25 09:14:55 +0530

    3f5a9a7kq: dockerized next js

--- server/next.config.mjs
+++ server/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'standalone',
   eslint: {
     // Disable ESLint during production builds
     ignoreDuringBuilds: true,
