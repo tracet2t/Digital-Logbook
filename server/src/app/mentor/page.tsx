3d0a4e2759c73e1890af1f2609e89df7afeb0e9c
arshvinw5 <tarshvin@gmail.com>
2026-03-28 17:23:13 +0530

    fix route bug for dashbaord

--- server/src/app/mentor/page.tsx
+++ server/src/app/mentor/page.tsx
@@ -1,11 +1,6 @@
 // app/mentor/page.tsx
-"use client";
-
-// This file needs to use some client-side logic for pop-up control
-//import MentorDashboard from "@/components/mentorDashboard";
+import { redirect } from "next/navigation";
 
 export default function MentorPage() {
-  return;
-
-  //<MentorDashboard />;
+  redirect("/mentor/dashboard");
 }
