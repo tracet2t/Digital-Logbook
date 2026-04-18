dbd91a97d1f604bfb66963d09087913b5849851e
Muditha <89349527+muditha20@users.noreply.github.com>
2026-04-18 15:32:06 +0530

    Fix import order in MenteeWarningCard component

--- server/src/components/mentor/MenteeWarningCard.tsx
+++ server/src/components/mentor/MenteeWarningCard.tsx
@@ -2,11 +2,11 @@
 
 import { useEffect, useRef, useState } from "react";
 
+import { useCreateWarning } from "@/_hooks/mentor/warninStatus";
 import { WarningCategory } from "@prisma/client";
 import { AlertCircle } from "lucide-react";
 import { toast } from "sonner";
 
-import { useCreateWarning } from "@/_hooks/mentor/warninStatus";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
