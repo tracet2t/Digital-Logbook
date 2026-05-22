dd6ea49f1a8e1920b522ade5e354e1847dbf6d1c
Muditha <89349527+muditha20@users.noreply.github.com>
2026-05-22 16:29:37 +0530

    update menteee task list pdf

--- server/src/repositories/user_repository_impl.ts
+++ server/src/repositories/user_repository_impl.ts
@@ -1,5 +1,7 @@
+import { User } from "@prisma/client";
+
 import prisma from "@/lib/prisma";
-import { User,} from "@prisma/client";
+
 import BaseRepository from "./baseRepository";
 
 /*
@@ -44,13 +46,10 @@ export class UserRepository extends BaseRepository<User> {
       },
     });
   }
-   async updateByEmail(email: string, data: Partial<User>) {
+  async updateByEmail(email: string, data: Partial<User>) {
     return this.modelClient.update({
       where: { email },
       data,
     });
   }
 }
-
-
-
